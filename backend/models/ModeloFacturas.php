af91cc4fe9a9487fc431d76b478940fd42439684
root <root@ConsolaFE.localdomain>
2017-07-21 17:59:38 +0000

    asd

---
+++ backend/models/ModeloFacturas.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "modelofactura".
+ *
+ * @property string $modeloid
+ * @property string $puntoventaid
+ * @property string $empresaid
+ * @property string $file
+ * @property integer $modelo
+ */
+class Modelofacturas extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'modelofactura';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['puntoventaid', 'empresaid', 'file', 'modelo'], 'required'],
+            [['puntoventaid', 'empresaid', 'modelo'], 'integer'],
+            [['file'], 'string', 'max' => 8000]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'modeloid' => 'Modeloid',
+            'puntoventaid' => 'Puntoventaid',
+            'empresaid' => 'Empresaid',
+            'file' => 'File',
+            'modelo' => 'Modelo',
+        ];
+    }
+    
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPuntoventa()
+    {
+    	return $this->hasOne(Puntosventa::className(), ['puntoventaid' => 'puntoventaid']);
+    }
+}
+
