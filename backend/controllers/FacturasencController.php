9adabc9e84869c1a167fd1171f431970518abff3
Matias Guidi <guidi.matias@gmail.com>
2017-07-21 10:01:25 -0300

    das

--- backend/controllers/FacturasencController.php
+++ backend/controllers/FacturasencController.php
@@ -854,6 +854,8 @@ class FacturasencController extends Controller
             array('modeloid' => '1','puntoventaid' => '29','empresaid' => '41','file' => 'uploads/41_0002.jpg','modelo' => '3')
             );
 
+        print_r($modelo);
+        exit();
 
     	$pv = PuntosventaSearch::getPuntoVentaEmpresaById($model->puntoventa);
 
